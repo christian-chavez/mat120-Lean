7783e6dd8211869058d84d7265ae4676d141eee3
Monica Garcia <Monica@air-de-monica.home>
2026-08-28 16:00:46 -0400

    Examples for Thomas during reunion

--- MAT120/Welcome.lean
+++ MAT120/Welcome.lean
@@ -5,3 +5,14 @@ open Verbose.French
 example (P Q : Prop) (hP : P) (hPQ : P → Q) : Q := by
   with_suggestions
     exact hPQ hP
+
+Exemple "Exemple 1.16 des notes de cours"
+ Données : (n : ℕ)
+ Hypothèses : (hdivisible : (∃ k : ℕ, n = k*4))
+ Conclusion : (∃ k' : ℕ, n = k*2)
+Démonstration :
+ Par hdivisible on obtient (l : ℕ) tel que (hl : n = 4*l)
+ Montrons que l*2 convient
+ On calcule
+QED
+/- Par hdivisible on obtient k tel que -/
